4e9676afdb842495904934ece7cc40ddef6e0dcd
Djadar <mukamaugusta5@gmail.com>
2020-09-30 08:52:02 +0100

    Revert "Revert "Revert "reception de la liste chainee"""
    This reverts commit 1e57bbefeffc2dea16fe842501d469e49a74ce9c.
    
    	modifié :         virt/kvm/kvm_main.c
    	modifié :         virt/kvm/policies.c

--- virt/kvm/policies.c
+++ virt/kvm/policies.c
@@ -11,71 +11,25 @@
 #include <linux/kvm.h>
 #include <linux/slab.h>
 
+//delete head
+page_pol_t * delete(page_pol_t *L);
+page_pol_t * delete(page_pol_t *L){
 
-//manage policy
-void kvm_apply_policy(struct kvm *kvm, page_pol_t *app);
-void kvm_apply_policy(struct kvm *kvm, page_pol_t *app){	
-	
-	struct kvm_dsm_memslots *slots;
-	struct kvm_dsm_memory_slot *slot;
-	struct kvm_dsm_info *info;
-
-	int i, j, k, N;
-	printk(KERN_INFO "ON ENTRE DANS LA FONCTION");
-
-	slots = __kvm_hvaslots(kvm);
-	printk(KERN_INFO "A- slots->used_slots = [%d]\n", slots->used_slots);
-	
-	struct kvm_page_pol act = app->data;
-	printk(KERN_INFO "AU Départ gpn = [%lu] et politique = {%c}", act.gpn, act.pol);
-	
-	j = 0;k = 0;i = 0;
-	
-	if (slots->used_slots != 0){
-		page_pol_t *tmp;
-		tmp = kzalloc(sizeof(page_pol_t), GFP_KERNEL);
-		
-		tmp = app;
-		//while(tmp !=NULL){
-			act = tmp->data;
-
-			//operations start here on act
-			bool flag1 = false;
-			printk(KERN_INFO "structure i = (%d) de gpn [%lu] et de politique {%c}",i,act.gpn,act.pol);
-			while (!flag1 && j < slots->used_slots) {
-				slot = &slots->memslots[j];
-				N = slot->npages;
-				long list_gpn[N];
-				bool flag2 = false;
-				while (!flag2 && k < N) {
-					info = &slot->vfn_dsm_state[k];
-		
-					list_gpn[k] = __kvm_dsm_vfn_to_gfn(slot, false,slot->base_vfn + k, 0, NULL);
-					if (list_gpn[k] == act.gpn){
-							printk(KERN_INFO "pour le slot j = (%d) et le npages - %d - , match (%lu)",j,k,act.gpn);
-							info->policy = act.pol;
-				
-							printk(KERN_INFO "\tgfn\tpol\n");
-							printk(KERN_INFO "\t[%llu]; \t %c;",list_gpn[k], info->policy);
-							printk(KERN_INFO "***************");
-							flag2 = true;
-							}else k = k + 1;
-					}
-				if (k < N) {flag1 = true;}
-				else {j = j + 1 ;}
-				}
-		//}
-		kfree(tmp);
-		/*kfree(slots);
-		kfree(slot);
-		kfree(info);*/
-	}
-	
+    page_pol_t *pel, *tmp;
+    if(L == NULL) return NULL;
+    else{
+        tmp = L;
+        L = tmp->next;
+        kfree(tmp);
+        return L;
+    }
 }
 
+
+
 //manage policy
-/*void kvm_apply_policy(struct kvm *kvm, page_pol_t *app);
-void kvm_apply_policy(struct kvm *kvm, page_pol_t *app){	
+void kvm_apply_policy(struct kvm *kvm, page_pol_t *app, page_pol_t *pel);
+void kvm_apply_policy(struct kvm *kvm, page_pol_t *app, page_pol_t *pel){	
 	
 	struct kvm_dsm_memslots *slots;
 	struct kvm_dsm_memory_slot *slot;
@@ -91,7 +45,16 @@ void kvm_apply_policy(struct kvm *kvm, page_pol_t *app){
 	printk(KERN_INFO "AU Départ gpn = [%lu] et politique = {%c}", act.gpn, act.pol);
 	
 	j = 0;k = 0;i = 0;
-	
+	//
+page_pol_t *tmp, *pel;
+		tmp = act;
+		pel = argp;
+		while(tmp !=NULL){
+			if (copy_from_user(tmp, pel , sizeof(page_pol_t)))
+				goto out;
+			tmp = tmp->next;
+			pel = pel->next;}
+//
 	if (slots->used_slots != 0){
 		page_pol_t *tmp;
 		tmp = kzalloc(sizeof(page_pol_t), GFP_KERNEL);
@@ -137,6 +100,6 @@ void kvm_apply_policy(struct kvm *kvm, page_pol_t *app){
 		/*kfree(slots);
 		kfree(slot);
 		kfree(info);*/
-	/*}
+	}
 	
-}*/
+}
